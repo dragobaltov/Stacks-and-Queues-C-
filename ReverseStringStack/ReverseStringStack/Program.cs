7f433a714590932dbb106210e18c7719379ed055
dragobaltov <dragobaltov@gmail.com>
2018-03-11 19:55:54 +0200

    add cs file

---
+++ ReverseStringStack/ReverseStringStack/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var stack = new Stack<char>(input);
+
+            foreach (var symbol in stack)
+            {
+                Console.Write(symbol);
+            }
+            Console.WriteLine();
+        }
+    }
+}
